307725f13d0a55ed5b99198dc19d1b82266cbb3d
Daniel Szlaski <daniel.szlaski@icloud.com>
2026-01-31 21:14:30 +0100

    Export ConfigLoader and EnvGuardConfig from configLoader for improved module accessibility

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { CodeScanner } from './scanner/codeScanner';
 export { EnvParser } from './parser/envParser';
 export { EnvAnalyzer } from './analyzer/envAnalyzer';
+export { ConfigLoader, EnvGuardConfig } from './config/configLoader';
 export { fixCommand } from './commands/fix';
 export * from './types';
