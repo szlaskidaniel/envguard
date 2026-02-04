bc8a95c03bbd96e622c8017fc8bb22d73c505655
Daniel Szlaski <daniel.szlaski@icloud.com>
2026-02-04 22:49:11 +0100

    Export install and uninstall hook commands from the index

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { EnvAnalyzer } from './analyzer/envAnalyzer';
 export { ConfigLoader, EnvGuardConfig, ConfigLoadResult } from './config/configLoader';
 export { fixCommand } from './commands/fix';
 export { scanCommand } from './commands/scan';
+export { installHookCommand, uninstallHookCommand } from './commands/install-hook';
 export { Logger } from './utils/logger';
 export * from './types';
 export { KNOWN_RUNTIME_VARS, isKnownRuntimeVar, getRuntimeVarCategory } from './constants/knownEnvVars';
